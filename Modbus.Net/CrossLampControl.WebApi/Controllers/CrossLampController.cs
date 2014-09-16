21a7b49388184c95aed973365fed0a6189fed3ab
parallelbgls@outlook.com <parallelbgls@outlook.com>
2014-09-16 13:21:28 +0800

    2014-09-16 update 1

--- Modbus.Net/CrossLampControl.WebApi/Controllers/CrossLampController.cs
+++ Modbus.Net/CrossLampControl.WebApi/Controllers/CrossLampController.cs
@@ -4,11 +4,44 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using CrossLampControl.WebApi.Models;
+using ModBus.Net;
 
 namespace CrossLampControl.WebApi.Controllers
 {
     public class CrossLampController : ApiController
     {
-        
+        ModbusUtility _utility = new ModbusUtility("COM6", (int)ModbusType.Rtu);
+        [HttpGet]
+        public Lamp GetLamp()
+        {
+            Lamp light = new Lamp();
+            bool[] lamps = _utility.GetCoils(2, "0", 6);
+            if (lamps[0])
+            {
+                light.MainLamp = LightLamp.Green.ToString();
+            }
+            else if (lamps[1])
+            {
+                light.MainLamp = LightLamp.Yellow.ToString();
+            }
+            else
+            {
+                light.MainLamp = LightLamp.Red.ToString();
+            }
+            if (lamps[3])
+            {
+                light.SubLamp = LightLamp.Green.ToString();
+            }
+            else if (lamps[4])
+            {
+                light.SubLamp = LightLamp.Yellow.ToString();
+            }
+            else
+            {
+                light.SubLamp = LightLamp.Red.ToString();
+            }
+            return light;
+        }
     }
 }
---
+++ Modbus.Net/CrossLampControl.WebApi/Models/Lamp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public enum LightLamp{Red,Yellow,Green}
+namespace CrossLampControl.WebApi.Models
+{
+    public class Lamp
+    {
+        public string MainLamp { get; set; }
+        public string SubLamp { get; set; }
+
+        public Lamp()
+        {
+            MainLamp = LightLamp.Red.ToString();
+            SubLamp = LightLamp.Red.ToString();
+        }
+
+        public Lamp(LightLamp mLamp, LightLamp sLamp)
+        {
+            MainLamp = mLamp.ToString();
+            SubLamp = sLamp.ToString();
+        }
+    }
+}
