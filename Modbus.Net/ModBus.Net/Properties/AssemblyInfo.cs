312d9ab0274239fa7cc5d6396da50762ab5f1a15
parallelbgls@outlook.com <parallelbgls@outlook.com>
2015-04-08 15:52:26 +0800

    2015-04-08 update 1 fix 1

--- Modbus.Net/ModBus.Net/Properties/AssemblyInfo.cs
+++ Modbus.Net/ModBus.Net/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.2.0.1016")]
-[assembly: AssemblyFileVersion("0.2.0.1016")]
+[assembly: AssemblyVersion("0.2.1.0408")]
+[assembly: AssemblyFileVersion("0.2.1.0408")]
