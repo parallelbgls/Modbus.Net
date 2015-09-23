3db6db2e1569b0dbaef0364fc6326adecde8a8a7
parallelbgls@outlook.com <parallelbgls@outlook.com>
2015-09-23 16:24:14 +0800

    reformat project architecture for more extends

--- Modbus.Net/ModBus.Net/AddressFormater.cs
+++ Modbus.Net/ModBus.Net/AddressFormater.cs
@@ -7,35 +7,11 @@ namespace ModBus.Net
         public abstract string FormatAddress(string area, int address);
     }
 
-    public class AddressFormaterSimense : AddressFormater
-    {
-        public override string FormatAddress(string area, int address)
-        {
-            if (area.Length > 1 && 
-                area.ToUpper().Substring(0,2) == "DB")
-            {
-                return area.ToUpper() + "." + "DB" + address;
-            }
-            else
-            {
-                return area.ToUpper() + address;
-            }
-        }
-    }
-
     public class AddressFormaterBase : AddressFormater
     {
         public override string FormatAddress(string area, int address)
         {
             return area + "." + address;
-        }
-    }
-
-    public class AddressFormaterNA200H : AddressFormater
-    {
-        public override string FormatAddress(string area, int address)
-        {
-            return area + address;
         }
     }
 }
---
+++ Modbus.Net/ModBus.Net/Simense/AddressFormaterSimense.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModBus.Net.Simense
+{
+    public class AddressFormaterSimense : AddressFormater
+    {
+        public override string FormatAddress(string area, int address)
+        {
+            if (area.Length > 1 &&
+                area.ToUpper().Substring(0, 2) == "DB")
+            {
+                return area.ToUpper() + "." + "DB" + address;
+            }
+            else
+            {
+                return area.ToUpper() + address;
+            }
+        }
+    }
+}
