cc3145f5d9108984189fa64c7210408b2a6dd32e
parallelbgls <parallelbgls@outlook.com>
2016-02-23 11:14:36 +0800

    2016-02-23 update 1 add nuspec files

--- Modbus.Net/Modbus.Net/Properties/AssemblyInfo.cs
+++ Modbus.Net/Modbus.Net/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
