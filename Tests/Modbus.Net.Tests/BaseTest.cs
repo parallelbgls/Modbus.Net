1e7e30bbcd6a08b994868e2dc073843d87589deb
parallelbgls <parallelbgls@outlook.com>
2017-01-16 10:30:21 +0800

    Tests Architechture.

--- Tests/Modbus.Net.Tests/BaseTest.cs
+++ Tests/Modbus.Net.Tests/BaseTest.cs
@@ -1,14 +1,50 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Modbus.Net.Tests
 {
-    [TestClass]
-    public class BaseTest
+    public sealed class TestAddresses
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Dictionary<string, Tuple<int, int, int>[]> Addresses { get; } = new Dictionary<string, Tuple<int, int, int>[]>();
+
+        public Tuple<int, int, int>[] this[string index]
         {
+            get { return Addresses[index]; }
+            set
+            {
+                if (!Addresses.ContainsKey(index))
+                {
+                    Addresses.Add(index, value);
+                }
+                else
+                {
+                    Addresses[index] = value;
+                }
+            }
+        }
+    }
+
+    public sealed class TestAreas
+    {
+        private Dictionary<string, string[]> Areas { get; } = new Dictionary<string, string[]>();
+
+        public string[] this[string index]
+        {
+            get { return Areas[index]; }
+            set
+            {
+                if (!Areas.ContainsKey(index))
+                {
+                    Areas.Add(index, value);
+                }
+                else
+                {
+                    Areas[index] = value;
+                }
+            }
         }
     }
 }
---
+++ Tests/Modbus.Net.Tests/CommunicationTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Modbus.Net.Tests
+{
+    [TestClass]
+    public class CommunicationTest
+    {
+        public void CommunicationInvoke(string addressFullName, int count)
+        {
+
+        }
+
+        [TestMethod]
+        public void CoilSingleRead()
+        {
+            
+        }
+
+        [TestMethod]
+        public void CoilMuiltiRead()
+        {
+            
+        }
+
+        [TestMethod]
+        public void CoilSingleWrite()
+        {
+
+        }
+
+        [TestMethod]
+        public void CoilMuiltiWrite()
+        {
+
+        }
+
+        [TestMethod]
+        public void RegisterSingleRead()
+        {
+            
+        }
+
+        [TestMethod]
+        public void RegistertMultiRead()
+        {
+            
+        }
+
+        [TestMethod]
+        public void RegisterSingleWrite()
+        {
+
+        }
+
+        [TestMethod]
+        public void RegistertMultiWrite()
+        {
+
+        }
+    }
+}
