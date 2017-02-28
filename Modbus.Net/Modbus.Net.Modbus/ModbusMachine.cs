b136bf252da202b090a0080b4f67db98ad3bddc4
parallelbgls <luoshengmstech@outlook.com>
2017-02-28 16:18:15 +0800

    2017-02-28 update 2 ModbusMachine Endian Add

--- Modbus.Net/Modbus.Net.Modbus/ModbusMachine.cs
+++ Modbus.Net/Modbus.Net.Modbus/ModbusMachine.cs
@@ -8,18 +8,18 @@ namespace Modbus.Net.Modbus
     public class ModbusMachine : BaseMachine
     {
         public ModbusMachine(ModbusType connectionType, string connectionString,
-            IEnumerable<AddressUnit> getAddresses, bool keepConnect, byte slaveAddress, byte masterAddress)
+            IEnumerable<AddressUnit> getAddresses, bool keepConnect, byte slaveAddress, byte masterAddress, Endian endian = Endian.BigEndianLsb)
             : base(getAddresses, keepConnect, slaveAddress, masterAddress)
         {
-            BaseUtility = new ModbusUtility(connectionType, connectionString, slaveAddress, masterAddress);
+            BaseUtility = new ModbusUtility(connectionType, connectionString, slaveAddress, masterAddress, endian);
             AddressFormater = new AddressFormaterModbus();
             AddressCombiner = new AddressCombinerContinus(AddressTranslator);
             AddressCombinerSet = new AddressCombinerContinus(AddressTranslator);
         }
 
         public ModbusMachine(ModbusType connectionType, string connectionString,
-            IEnumerable<AddressUnit> getAddresses, byte slaveAddress, byte masterAddress)
-            : this(connectionType, connectionString, getAddresses, false, slaveAddress, masterAddress)
+            IEnumerable<AddressUnit> getAddresses, byte slaveAddress, byte masterAddress, Endian endian = Endian.BigEndianLsb)
+            : this(connectionType, connectionString, getAddresses, false, slaveAddress, masterAddress, endian)
         {
         }
     }
