0936a28d7a37b1b17c270842e7c48d7e8d252f5c
parallelbgls <parallelbgls@outlook.com>
2017-04-10 16:49:02 +0800

    2017-04-10 Update 2 Package

--- Modbus.Net/src/Base.Common/AsyncHelper.cs
+++ Modbus.Net/src/Base.Common/AsyncHelper.cs
@@ -1,15 +1,12 @@
-﻿/* AsyncHelper 注释
- * -- AsyncHelper来自于AsyncEx，为了引用方便直接拷贝了代码。Modbus.Net的作者不保留对AsyncHeloper类的版权。
- * -- AsyncHelper copied from AsyncEx. The author of "Modbus.Net" <b>donnot</b> obtain the copyright of AsyncHelper(Only).
- */
-
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Modbus.Net
 {
+    /// <summary>
+    /// AsyncHelper Class
+    /// </summary>
     public static class AsyncHelper
     {
         private static readonly TaskFactory _myTaskFactory = new
@@ -62,108 +59,6 @@ namespace Modbus.Net
             CancellationToken token)
         {
             return task.ContinueWith(t => t.GetAwaiter().GetResult(), token);
-        }
-    }
-
-    /// <summary>
-    ///     AsyncLock locks across one or several await calls.
-    /// </summary>
-    public class AsyncLock
-    {
-        private readonly Task<Releaser> _releaser;
-        private readonly AsyncSemaphore _semaphore;
-
-        public AsyncLock()
-        {
-            _semaphore = new AsyncSemaphore(1);
-            _releaser = Task.FromResult(new Releaser(this));
-        }
-
-        /// <summary>
-        ///     Lock the async method. Call like: using (await asynclock.LockAsync())
-        /// </summary>
-        /// <returns></returns>
-        public Task<Releaser> LockAsync()
-        {
-            var wait = _semaphore.WaitAsync();
-            return wait.IsCompleted
-                ? _releaser
-                : wait.ContinueWith((_, state) => new Releaser((AsyncLock) state),
-                    this, CancellationToken.None,
-                    TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
-        }
-
-
-        public struct Releaser : IDisposable
-        {
-            private readonly AsyncLock _toRelease;
-
-            internal Releaser(AsyncLock toRelease)
-            {
-                _toRelease = toRelease;
-            }
-
-            public void Dispose()
-            {
-                if (_toRelease != null)
-                {
-                    _toRelease._semaphore.Release();
-                }
-            }
-        }
-    }
-
-    /// <summary>
-    ///     AsyncSemaphore semaphore the multi run tasks.
-    /// </summary>
-    public class AsyncSemaphore
-    {
-        private static readonly Task _completed = Task.FromResult(true);
-        private readonly Queue<TaskCompletionSource<bool>> _waiters = new Queue<TaskCompletionSource<bool>>();
-        private int _currentCount;
-
-        public AsyncSemaphore(int initialCount)
-        {
-            if (initialCount < 0)
-            {
-                throw new ArgumentOutOfRangeException("initialCount");
-            }
-            _currentCount = initialCount;
-        }
-
-        public Task WaitAsync()
-        {
-            lock (_waiters)
-            {
-                if (_currentCount > 0)
-                {
-                    _currentCount--;
-                    return _completed;
-                }
-                var waiter = new TaskCompletionSource<bool>();
-                _waiters.Enqueue(waiter);
-                return waiter.Task;
-            }
-        }
-
-        public void Release()
-        {
-            TaskCompletionSource<bool> toRelease = null;
-            lock (_waiters)
-            {
-                if (_waiters.Count > 0)
-                {
-                    toRelease = _waiters.Dequeue();
-                }
-                else
-                {
-                    _currentCount++;
-                }
-            }
-            if (toRelease != null)
-            {
-                toRelease.SetResult(true);
-            }
         }
     }
 }
