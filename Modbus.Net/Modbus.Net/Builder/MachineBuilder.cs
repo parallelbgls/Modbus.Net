baa29e91bb0f3f7bc3925b608b9bce71b8880708
parallelbgls <parallelbgls@outlook.com>
2018-10-14 13:25:44 +0900

    2018-10-14 Update 1 Builder Class Add

---
+++ Modbus.Net/Modbus.Net/Builder/MachineBuilder.cs
@@ -0,0 +1,16 @@
+﻿//Machine Builder. Build a machine without constructor.
+//MachineBuiler.CreateMachine.(SetAddresses.SetFormatter.SetCombiner).(CreateUtility.SetTranslator).SetEndian.SetProtocolGroups.SetLinker.SetConector.SetController.Build
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modbus.Net.Builder
+{
+    public class MachineBuilder
+    {
+
+    }
+}
